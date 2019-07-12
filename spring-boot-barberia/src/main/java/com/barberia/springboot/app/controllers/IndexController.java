66204424e0126eade4f9c56be816b201bb18c79e
ckouta <ckouta25@gmail.com>
2019-07-12 00:31:00 -0400

    horario disponible

--- spring-boot-barberia/src/main/java/com/barberia/springboot/app/controllers/IndexController.java
+++ spring-boot-barberia/src/main/java/com/barberia/springboot/app/controllers/IndexController.java
@@ -1,13 +1,22 @@
 package com.barberia.springboot.app.controllers;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.barberia.springboot.app.models.entity.Barbero;
+import com.barberia.springboot.app.models.entity.BloqueHorario;
 import com.barberia.springboot.app.models.service.IBarberoService;
 import com.barberia.springboot.app.models.service.IBloqueHorarioService;
 import com.barberia.springboot.app.models.service.IServicioService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class IndexController {
@@ -29,5 +38,6 @@ public class IndexController {
 		model.addAttribute("horarios", bh.findAll());
 		return "index";
 	}
+
 	
 }
